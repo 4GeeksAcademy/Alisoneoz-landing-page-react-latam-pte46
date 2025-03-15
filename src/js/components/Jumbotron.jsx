29359897ef7453c6ef621094d97a133902ded855
Alison Estephany <96319139+Alisoneoz@users.noreply.github.com>
2025-03-15 02:15:22 +0000

    initial commit

---
+++ src/js/components/Jumbotron.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Jumbotron = () => {
+    return (
+        <div className="bg-light text-start">
+            <h1>A warm welcome</h1>
+            <p></p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    );
+};
---
+++ src/js/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export const Navbar = () => {
+	return (
+
+<nav className="navbar navbar-expand-lg bg-dark">
+  <div className="container-fluid d-flex justify-content-between"> 
+    <a className="navbar-brand text-light " href="#">StartBootstrap</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+      <ul className="navbar-nav ">
+        <li className="nav-item">
+          <a className="text-light nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="text-light  nav-link text-light " href="#">Features</a>
+        </li>
+        <li className="nav-item">
+          <a className="text-light nav-link" href="#">Pricing</a>
+        </li>
+        <li className="nav-item">
+          <a className="text-light nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+	);
+};
