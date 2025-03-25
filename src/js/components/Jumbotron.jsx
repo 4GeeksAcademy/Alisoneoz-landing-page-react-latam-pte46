ca6561c0b634efebd79cffe89bce6ab554370147
Alison Estephany <96319139+Alisoneoz@users.noreply.github.com>
2025-03-25 23:18:25 +0000

    Adjsut className attribute

--- src/js/components/Jumbotron.jsx
+++ src/js/components/Jumbotron.jsx
@@ -5,7 +5,7 @@ export const Jumbotron = () => {
         <div className="bg-light text-start p-5">
             <h1>A warm welcome</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-            <a href="#" class="btn btn-primary">Call to Action</a>
+            <a href="#" className="btn btn-primary">Call to Action</a>
         </div>
     );
 };
